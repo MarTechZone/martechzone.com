c6978fe71b4510bf066e879b036bd583f5815118
Steven Rose <steven.rose@infusionsoft.com>
2016-03-19 13:55:41 -0700

    Initial commit with Infusionsoft form on landing page and analytics

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MarTech Zone | Heart of Marketing Technology</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/normalize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.min.css">
+    <link href='http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/foundation.css" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+    <style>
+    .top-bar {background-color: #6699cc;height: 7.5rem;}
+    @media only screen and (min-width: 40.0625em)
+      .top-bar {background-color: #6699cc;}
+    </style>
+
+    <?php include_once('analytics.php'); ?>
+  </head>
+
+  <body>
+    <nav class="top-bar" data-topbar>
+      <h1><img src="./imgs/MTZ-logo-white.png" style="width: 500px;display: block;margin: 0 auto;padding-top: 0.5rem;"></h1>
+    </nav>
+
+    <div class="row">
+      <div class="large-8 columns">
+        <h2 style="padding-top: 1.5rem;margin-bottom: 1rem;">Welcome to the MarTech Zone!</h2>
+        <p>Is managing your MarTech a challenge? Do you have people focused on maintaining, upgrading and improving your Marketing Technology in your company? Do you have a process in place for researching and vetting MarTech providers to determine which is best for your business?</p>
+        <p>Many exciting things to come from the MarTech Zone. Sign-up to stay up-to-date and be the first to know when there are new announcements.</p>
+        <!-- p>We'd love to hear from you. You can either reach out to us as a whole and one of our awesome team members will get back to you, or if you have a specific question reach out to one of our staff. We love getting email all day <em>all day</em>.</p -->
+      </div>
+
+      <div class="large-4 columns">
+        <section class="section" style="background-color: #d9e6f2;padding: 1rem;">
+          <h3 class="title" style="padding-top: 1rem;color: #6699cc;">Stay Up-to-Date!</h3>
+          <div class="content" data-slug="panel1">
+            <form accept-charset="UTF-8" action="https://qz130.infusionsoft.com/app/form/process/3df60d82a89dc1ba1ae9474301901ac8" class="infusion-form" method="POST">
+              <label class="" for="inf_field_FirstName">First Name *</label>
+              <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="text" />
+
+              <label class="" for="inf_field_Email">Business Email *</label>
+              <input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="text" />
+
+              <input name="inf_form_xid" type="hidden" value="3df60d82a89dc1ba1ae9474301901ac8" />
+              <input name="inf_form_name" type="hidden" value="Register for Newsletter" />
+              <input name="infusionsoft_version" type="hidden" value="1.51.0.57" />
+              <button type="submit" class="radius button" style="background-color: #264d73;display: block;margin: 0 auto;">Send Me Latest Info</button>
+            </form>
+            <script type="text/javascript" src="https://qz130.infusionsoft.com/app/webTracking/getTrackingCode?trackingId=57eed9560e53636b64e4d469d1b663ed"></script>
+          </div>
+        </section>
+      </div>
+    </div>
+
+    <footer class="row">
+      <div class="large-12 columns">
+      <hr style="margin-top: 0;" />
+        <div class="row">
+          <div class="large-6 columns">
+            <p>&copy; 2016 MarTech Zone, LLC</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/js/foundation.min.js"></script>
+    <script>$(document).foundation();</script>
+
+  </body>
+</html>
