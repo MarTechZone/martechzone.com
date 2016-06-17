74ca232022aa48360d83c5e9992573c7437ce1fd
Steven Rose <steven.rose@infusionsoft.com>
2016-06-17 08:13:37 -0700

    Updated logo/branding

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
     <style>
-    .top-bar {background-color: #6699cc;height: 7.5rem;}
+    .top-bar {background: #fff;border-bottom: 8px solid #414142;height: 8rem;}
     @media only screen and (min-width: 40.0625em)
-      .top-bar {background-color: #6699cc;}
+      .top-bar {background: #fff;border-bottom: 8px solid #414142;}
     </style>
 
     <?php include_once('analytics.php'); ?>
@@ -24,20 +24,20 @@
 
   <body>
     <nav class="top-bar" data-topbar>
-      <h1><img src="./imgs/MTZ-logo-white.png" style="width: 500px;display: block;margin: 0 auto;padding-top: 0.5rem;"></h1>
+      <h1><img src="./imgs/MartechZone-100h" style="width: 521px;display: block;margin: 0 auto;padding-top: 0.5rem;"></h1>
     </nav>
 
     <div class="row">
       <div class="large-8 columns">
         <h2 style="padding-top: 1.5rem;margin-bottom: 1rem;">Welcome to the MarTech Zone!</h2>
-        <p>Is managing your MarTech a challenge? Do you have people focused on maintaining, upgrading and improving your Marketing Technology in your company? Do you have a process in place for researching and vetting MarTech providers to determine which is best for your business?</p>
+        <p>Is managing your all your Marketing Technologies a challenge? Do you have people focused on maintaining, upgrading and improving your Marketing Technology in your company? Do you have a process in place for researching and vetting MarTech providers to determine which is best for your business?</p>
         <p>Many exciting things to come from the MarTech Zone. Sign-up to stay up-to-date and be the first to know when there are new announcements.</p>
         <!-- p>We'd love to hear from you. You can either reach out to us as a whole and one of our awesome team members will get back to you, or if you have a specific question reach out to one of our staff. We love getting email all day <em>all day</em>.</p -->
       </div>
 
       <div class="large-4 columns">
-        <section class="section" style="background-color: #d9e6f2;padding: 1rem;">
-          <h3 class="title" style="padding-top: 1rem;color: #6699cc;">Stay Up-to-Date!</h3>
+        <section class="section" style="background-color: #c1c0c0;padding: 1rem;">
+          <h3 class="title" style="padding-top: 1rem;">Stay Up-to-Date!</h3>
           <div class="content" data-slug="panel1">
             <form accept-charset="UTF-8" action="https://qz130.infusionsoft.com/app/form/process/3df60d82a89dc1ba1ae9474301901ac8" class="infusion-form" method="POST">
               <label class="" for="inf_field_FirstName">First Name *</label>
@@ -49,7 +49,7 @@
               <input name="inf_form_xid" type="hidden" value="3df60d82a89dc1ba1ae9474301901ac8" />
               <input name="inf_form_name" type="hidden" value="Register for Newsletter" />
               <input name="infusionsoft_version" type="hidden" value="1.51.0.57" />
-              <button type="submit" class="radius button" style="background-color: #264d73;display: block;margin: 0 auto;">Send Me Latest Info</button>
+              <button type="submit" class="radius button" style="background-color: #f69321;display: block;margin: 0 auto;">Send Me Latest Info</button>
             </form>
             <script type="text/javascript" src="https://qz130.infusionsoft.com/app/webTracking/getTrackingCode?trackingId=57eed9560e53636b64e4d469d1b663ed"></script>
           </div>
