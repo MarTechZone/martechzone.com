964e2a249926989f8124a26b7d5ba5e0b7265412
Steven Rose <StevenR511@gmail.com>
2016-10-11 15:22:00 -0700

    Content expansion and images added for Service focus

--- index.php
+++ index.php
@@ -11,13 +11,28 @@
     <?php include_once('analytics.php'); ?>
 
     <style>
+/*
+Orange: f69321
+Light Orange: fbcf9d
+
+Blue: 00aeef
+Light Blue: 99e4ff
+
+Purple: 660066
+Light Purple: ff99ff
+*/
+      .orange {color: #f69321;}
+      .blue {color: #00aeef;}
+      .purple {color: #660066;}
+      .red {color: #ff0000;}
+
       a {color: #f69321;}
-      a:hover {color: #dc7a09;}
+      a:hover {color: #fbcf9d;}
 
       .button {background: #f69321;}
       .button.hollow {color: #f69321;border-color: #f69321;}
-      .button:hover {background: #dc7a09;}
-      .button.hollow:hover {color: #dc7a09;border-color: #dc7a09;}
+      .button:hover {background: #fbcf9d;}
+      .button.hollow:hover {color: #fbcf9d;border-color: #fbcf9d;}
 
       dl, ol, ul {list-style-position: inside;}
     </style>
@@ -28,7 +43,7 @@
     <nav class="top-bar">
       <div class="top-bar-left">
         <ul class="menu">
-          <li class="menu-text"><img src="./imgs/MartechZone-100h.png" style="max-height: 40px;"></li>
+          <li class="menu-text"><a href="/" style="padding: 0;"><img src="./imgs/MartechZone-100h.png" style="max-height: 40px;"></a></li>
         </ul>
       </div>
       <div class="top-bar-right">
@@ -40,7 +55,7 @@
     </nav>
     <!-- End Top Bar -->
 
-    <div class="callout large" style="margin: 0;">
+    <div class="callout large" style="">
       <div class="row column text-center">
         <h1>Changing the World Through Measured Experiences</h1>
         <p class="lead">Innovate technology experiences that deliver value and delight.</p>
@@ -49,9 +64,9 @@
       </div>
     </div>
 
-    <div class="row" style="background-color: #fde7ce;padding-top: 2.5rem;">
+    <div class="row" style="padding-top: 2.5rem;">
       <div class="medium-6 columns medium-push-6">
-        <img class="thumbnail" src="http://placehold.it/750x350">
+        <img class="thumbnail" src="./imgs/Data-Strategy-Tech-User.png">
       </div>
       <div class="medium-6 columns medium-pull-6">
         <h2><a name="solutions"></a>Business Goal Solutions</h2>
@@ -59,10 +74,10 @@
       </div>
     </div>
 
-    <div class="row" style="background-color: #fde7ce;padding: 1rem 0 1.5rem 0;">
-      <div class="medium-4 columns">
-        <h3>Data/Analytics</h3>
-        <p>We utilize data to our advantage.</p>
+    <div class="row" style="background-color: #eee;margin: 2rem 0;padding: 2.5rem 0 1.5rem 0;">
+      <div class="medium-5 columns">
+        <h3 class="orange"><img class="" src="./imgs/data.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">Data/Analytics</h3>
+        <h5>We utilize data to our advantage</h5>
         <ul>
           <li>Data Measurement Benchmark</li>
           <li>Analytics Gap Analysis</li>
@@ -70,9 +85,20 @@
           <li>Analytics Deep Dive</li>
         </ul>
       </div>
-      <div class="medium-4 columns">
-        <h3>Strategy</h3>
-        <p>We align to solve business goals.</p>
+      <div class="medium-7 columns">
+        <p>Starting with the objective "what are you trying to accomplish" is the cornerstone to every project. If you don't have a goals, KPIs, and baseline data, how are you going to know that a project is successful or not?</p>
+        <p>Do you keep spending money on it or drop it? A goal of "get more leads" is a starting point, but how many leads? When do you need them by for this project to be successful? Is your measurement of success realistic based on historical trends? Are you gathering the necessary data points to be able to accurately show success or failure of the project?</p>
+      </div>
+    </div>
+
+    <div class="row" style="padding: 2.5rem 0 1.5rem 0;">
+      <div class="medium-7 columns">
+        <p>Stepping back and taking a holistic view helps to ensure various sites or initiatives are helping build the business as a whole, not fighting each other for the same resources or visitors.</p>
+        <p>A user of your site or product has many options on next steps to take. Planning out the ideal path you'd like them to take as well as alternate options if your optimal path isn't what they need, is essential for a good website flow. If you overlay this big picture mindset through your entire ecosystem (social platforms, landing pages, other sites/pages, product(s), etc.), you can have multiple user funnels that can help your various user types (prospects, customers, media, career seekers, employees, etc.) find what they need when they need it. Delivering this kind of value to your user increases trust in your brand and brings them back to you for the source of information.</p>
+      </div>
+      <div class="medium-5 columns">
+        <h3 class="blue"><img class="" src="./imgs/strategy.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">Strategy</h3>
+        <h5>We align to solve business goals</h5>
         <ul>
           <li>Website Flow Analysis</li>
           <li>Customer Analytics and Insights</li>
@@ -80,9 +106,12 @@
           <li>Lead Scoring Roadmap</li>
         </ul>
       </div>
-      <div class="medium-4 columns">
-        <h3>Technology</h3>
-        <p>We leverage software to scale.</p>
+    </div>
+
+    <div class="row" style="background-color: #eee;margin: 2rem 0;padding: 2.5rem 0 1.5rem 0;">
+      <div class="medium-5 columns">
+        <h3 class="purple"><img class="" src="./imgs/technology.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">Technology</h3>
+        <h5>We leverage software to scale</h5>
         <ul>
           <li>Digital Audit</li>
           <li>Platform Integrations</li>
@@ -90,17 +119,53 @@
           <li>CRM &amp; Marketing Automation</li>
         </ul>
       </div>
+      <div class="medium-7 columns">
+        <p>Technology at it's base isn't a solution, once there is a working strategy or process, then purchasing or leveraging technology to scale is where businesses get the best use of any software.</p>
+        <p>Marktech technologies (MarTech) systems sound amazing, it helps that they're based in marketing, but adding a technology to your stack to enable online chatting, personalized content, or optimization testing for instance need to fit into an overall MarTech plan for your technology stack. If your chat system doesn't integrate with your CRM (Customer Relationship Management) or Marketing Automation system, then someone on your team is either having to do a bunch of manual work or you're left with a lot of incomplete data.</p>
+      </div>
     </div>
 
     <div class="row" style="margin-top: 2.5rem;">
-      <div class="medium-7 large-7 columns">
+      <div class="medium-12 columns">
         <h2><a name="solve"></a>What are you trying to solve?</h2>
-        <p>Do you have a large budget tied up in technology and not sure that it's working for you? Is your Marketing Technology stack integrated and aligning with your business goals? Does your I.T. group collaborate with your MarTech group? Are you trying to personalize your users experience but not sure how or where to start?</p>
-        <p>Are you struggling to improve your organic traffic? Do you have insights into each piece of content you produce? Do the leads that are getting into your system being routed to the correct sales rep? Are your sales reps wasting time talking to the wrong people in your funnel? Is your funnel working like a well oiled machine?</p>
-        <p>Do you know where you're problems are located? Can you get insights from the data you're capturing from your website? When you run a campaign, can you measure the success/failure of it? Are visitors responding to your landing pages or bouncing off at alarming rates?</p>
       </div>
+      <div class="medium-6 columns">
+        <ul>
+          <li>Are you getting <strong>ROI</strong> from your technology investment?</li>
+          <li><strong>MarTech stack</strong> integrated and aligning to business goals?</li>
+          <li>Does your I.T. group <strong>collaborate</strong> with your MarTech group?</li>
+          <li>Wanting to begin <strong>personalizing your users experience</strong>?</li>
+          <li>Do you know where you're <strong>problems</strong> are located?</li>
+          <li>Can you get insights from your <strong>website</strong> visitors?</li>
+          <li>Are visitors responding to your <strong>landing pages</strong>?</li>
+        </ul>
+      </div>
+      <div class="medium-6 columns">
+        <ul>
+          <li>Are you struggling to improve your <strong>organic traffic</strong>?</li>
+          <li>Do you have <strong>insights</strong> into each piece of your <strong>content</strong>?</li>
+          <li>Are your <strong>leads being routed</strong> to the correct sales rep?</li>
+          <li>Are sales reps wasting time talking to the <strong>wrong people</strong>?</li>
+          <li>Is your <strong>funnel</strong> working like a well oiled machine?</li>
+          <li>Can you measure the <strong>success/failure of a campaign</strong>?</li>
+          <li>Are you <strong>testing</strong> to see what your audience responds too?</li>
+        </ul>
+      </div>
+    </div>
 
-      <div class="medium-5 large-5 columns">
+    <div class="row" style="background-color: #eee;margin: 2rem 0;padding: 2.5rem 0 1.5rem 0;">
+      <div class="medium-7 columns">
+        <h3 class="red"><img class="" src="./imgs/user.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">User Focus</h3>
+        <ul>
+          <li>Spotlight on your users and their experiences</li>
+          <li>Get out of the company headspace and into your users</li>
+          <li>Solve for users needs as they align to business needs</li>
+        </ul>
+        <p>A user doesn't care how you're company is setup, but more often then not their experience of your company is broken and inconsistent because it's custom to whatever part of the business "owns" that section of the users experience.</p>
+        <p>Users experience your brand in many different ways. Do you have a consistent messaging and visual brand defined? If a visitor contact your business on social media, then went to the website, and then received an email, would it feel like they were in connection with the same company?</p>
+        <p>Studies show that <a href="http://www.uxbooth.com/articles/improve-conversions-with-better-personalization/" target="_blank">75% of consumers prefer when brands personalize</a> messages and offers, how does a business accomplish that if you're not capturing what the user is doing, integrating that information together, and making decisions based off those actions. If <a href="http://www.uxbooth.com/articles/improve-conversions-with-better-personalization/" target="_blank">70% of online consumers get frustrated</a> when promotional items they're offered has nothing to do with their interests, you're doing more harm then good by upsetting them by pushing generic promos into their experience.</p>
+      </div>
+      <div class="medium-5 columns">
         <div class="callout secondary" style="background-color: #c1c0c0;">
           <h3 class="title" style=""><a name="help"></a>How can we help?</h3>
           <form lpformnum="1" accept-charset="UTF-8" action="https://qz130.infusionsoft.com/app/form/process/3df60d82a89dc1ba1ae9474301901ac8" class="infusion-form" method="POST">
@@ -117,7 +182,7 @@
               </div>
               <div class="small-12 columns">
                 <label class="">Message *
-                  <textarea class="" rows="5"></textarea>
+                  <textarea class="" rows="7" placeholder="Please help me help my users."></textarea>
                 </label>
 
                 <input name="inf_form_xid" type="hidden" value="3df60d82a89dc1ba1ae9474301901ac8" />
@@ -133,8 +198,8 @@
       </div>
     </div>
 
-    <footer class="row column" style="background-color: #eee;padding: .5rem;margin-top: 2rem;">
-      <p style="padding: 1rem 0 0 1rem;">&copy; <?php echo date('Y'); ?> MarTech Zone, LLC</p>
+    <footer class="row column" style="padding: .5rem;">
+      <p style="padding-left: 1rem;">&copy; <?php echo date('Y'); ?> MarTech Zone, LLC</p>
     </footer>
 
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
