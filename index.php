3c0b01777285fa9f70fcd04384a58f904a5afb86
Steven Rose <StevenR511@gmail.com>
2016-10-27 20:10:46 -0700

    Trimming of services for more informational site

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
     <?php include_once('analytics.php'); ?>
 
@@ -43,6 +44,9 @@ Light Purple: ff99ff
       .purple {color: #660066;}
       .red {color: #ff0000;}
       .bkgd-gray {background-color: #f0f0f0;}
+
+      body, h1, h2, h3, h4, h5, h6 {font-family: 'Lato', sans-serif;}
+      p, li {font-size: 1.15rem;line-height: 2rem;}
 
       a {color: #f69321;}
       a:hover {color: #fbcf9d;}
@@ -69,7 +73,6 @@ Light Purple: ff99ff
       </div>
       <div class="top-bar-right">
         <ul class="menu" style="">
-          <li class="menu-text"><a href="#solutions">Solutions</a></li>
           <li class="menu-text"><a href="#help" class="button">Contact</a></li>
         </ul>
       </div>
@@ -79,9 +82,7 @@ Light Purple: ff99ff
     <div class="callout large">
       <div class="row column text-center">
         <h1>Changing the World Through Measured Experiences</h1>
-        <p class="lead">Innovate technology experiences that deliver value and delight.</p>
-        <a href="#help" class="button large">Contact for Assistance</a>
-        <a href="#solutions" class="button large hollow">View Services</a>
+        <h4>Innovate technology experiences that deliver value and delight</h4>
       </div>
     </div>
 
@@ -91,7 +92,7 @@ Light Purple: ff99ff
           <img class="thumbnail" src="./imgs/Data-Strategy-Tech-User.png">
         </div>
         <div class="medium-6 columns medium-pull-6">
-          <h2><a name="solutions"></a>Business Goal Solutions</h2>
+          <h2>Business Goal Solutions</h2>
           <p>We are a team of technically minded, data-driven marketers with years of experience in the digital space. Our diverse experience solving complex issues at scale in the fast moving technology space will help you solve the key elements that are getting in your way of being successful.</p>
         </div>
       </div>
@@ -99,107 +100,48 @@ Light Purple: ff99ff
 
     <div class="bkgd-gray" style="padding: 3rem 0; 2.5rem">
       <div class="row">
-        <div class="medium-5 columns">
+        <div class="medium-4 columns">
           <h3 class="orange"><img class="" src="./imgs/data.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">Data/Analytics</h3>
           <h5>We utilize data to our advantage</h5>
           <ul>
-            <li>Data Measurement Benchmark</li>
             <li>Analytics Gap Analysis</li>
-            <li>Reporting Capability</li>
-            <li>Analytics Deep Dive</li>
+            <li>Dashboarding Capability</li>
+            <li>Data Quality Management (DQM)</li>
+            <li>Establish KPI Benchmarks</li>
           </ul>
         </div>
-        <div class="medium-7 columns">
-          <p>Starting with the objective "what are you trying to accomplish" is the cornerstone of every project. If you don't have goals, KPIs, and baseline data, how are you going to know that a project is successful or not?</p>
-          <p>Do you keep spending money on it or drop it? A goal of "get more leads" is a starting point, but how many leads? When do you need them by for this project to be successful? Is your measurement of success realistic based on historical trends? Are you gathering the necessary data points to be able to accurately show success or failure of the project?</p>
-        </div>
-      </div>
-    </div>
-
-    <div style="padding: 3rem 0; 2.5rem;">
-      <div class="row">
-        <div class="medium-5 columns">
+        <div class="medium-4 columns">
           <h3 class="blue"><img class="" src="./imgs/strategy.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">Strategy</h3>
-          <h5>We align to solve business goals</h5>
+          <h5>We align to your business goals</h5>
           <ul>
-            <li>Website Flow Analysis</li>
-            <li>Customer Analytics and Insights</li>
-            <li>Omni-Channel Strategy</li>
+            <li>Conversion Optimization</li>
+            <li>Demand Funnel Taxonomy</li>
             <li>Lead Scoring Roadmap</li>
+            <li>Website Flow Mapping</li>
           </ul>
         </div>
-        <div class="medium-7 columns">
-          <p>Stepping back and taking a holistic view helps to ensure various sites or initiatives are helping build the business as a whole, not fighting each other for the same resources or visitors.</p>
-          <p>A user of your site or product has many options on next steps to take. Planning out the ideal path you'd like them to take as well as alternate options if your optimal path isn't what they need, is essential for a good website flow. If you overlay this big picture mindset through your entire ecosystem (social platforms, landing pages, other sites/pages, product(s), etc.), you can have multiple user funnels that can help your various user types (prospects, customers, media, career seekers, employees, etc.) find what they need when they need it. Delivering this kind of value to your user increases trust in your brand and brings them back to you for the source of information.</p>
-        </div>
-      </div>
-    </div>
-
-  <div class="bkgd-gray" style="padding: 3rem 0; 2.5rem;">
-      <div class="row">
-        <div class="medium-5 columns">
+        <div class="medium-4 columns">
           <h3 class="purple"><img class="" src="./imgs/technology.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">Technology</h3>
           <h5>We leverage software to scale</h5>
           <ul>
-            <li>Digital Audit</li>
-            <li>Platform Integrations</li>
-            <li>Demand Funnel Services</li>
+            <li>Content Personalization</li>
             <li>CRM &amp; Marketing Automation</li>
-          </ul>
-        </div>
-        <div class="medium-7 columns">
-          <p>Technology at it's base isn't a solution, once there is a working strategy or process, then purchasing or leveraging technology to scale is where businesses get the best use of any software.</p>
-          <p>Marketing technologies (MarTech) systems sound amazing, it helps that they're based on marketing, but adding a technology to your stack to enable online chatting, personalized content or optimization testing, for instance, need to fit into an overall MarTech plan for your technology stack. If your chat system doesn't integrate with your CRM (Customer Relationship Management) or Marketing Automation system, then someone on your team is either having to do a bunch of manual work or you're left with a lot of incomplete data.</p>
-        </div>
-      </div>
-    </div>
-
-    <div style="padding: 3rem 0; 2.5rem">
-      <div class="row">
-        <div class="medium-12 columns">
-          <h2><a name="solve"></a>What are you trying to solve?</h2>
-        </div>
-        <div class="medium-6 columns">
-          <ul>
-            <li>Are you getting <strong>ROI</strong> from your technology investment?</li>
-            <li><strong>MarTech stack</strong> integrated and aligning to business goals?</li>
-            <li>Does your I.T. group <strong>collaborate</strong> with your MarTech group?</li>
-            <li>Wanting to begin <strong>personalizing your user's experience</strong>?</li>
-            <li>Do you know where you're <strong>problems</strong> are located?</li>
-            <li>Can you get insights from your <strong>website</strong> visitors?</li>
-            <li>Are visitors responding to your <strong>landing pages</strong>?</li>
-          </ul>
-        </div>
-        <div class="medium-6 columns">
-          <ul>
-            <li>Are you struggling to improve your <strong>organic traffic</strong>?</li>
-            <li>Do you have <strong>insights</strong> into each piece of your <strong>content</strong>?</li>
-            <li>Are your <strong>leads being routed</strong> to the correct sales rep?</li>
-            <li>Are sales reps wasting time talking to the <strong>wrong people</strong>?</li>
-            <li>Is your <strong>funnel</strong> working like a well-oiled machine?</li>
-            <li>Can you measure the <strong>success/failure of a campaign</strong>?</li>
-            <li>Are you <strong>testing</strong> to see what your audience responds too?</li>
+            <li>Platform Integrations</li>
+            <li>Technology Stack Audit</li>
           </ul>
         </div>
       </div>
     </div>
 
-    <div class="bkgd-gray" style="padding: 2.5rem 0;">
+    <div class="" style="padding: 3.5rem 0 1rem;">
       <div class="row">
         <div class="medium-7 columns">
           <h3 class="red"><img class="" src="./imgs/user.png" style="padding: 0 0.75rem 0.5rem 0;max-height: 60px;vertical-align: middle;">User Focus</h3>
-          <ul>
-            <li>Spotlight on your users and their experiences</li>
-            <li>Get out of the company headspace and into your users</li>
-            <li>Solve for users needs as they align to business needs</li>
-          </ul>
-          <p>A user doesn't care how the company is structured, but more often than not their experience of your company is broken and inconsistent because it's custom to whatever part of the business "owns" that section of the user's experience.</p>
-          <p>Users experience your brand in many different ways. Do you have a consistent messaging and visual brand defined? If a visitor contact's your business on social media, then went to the website, and then received an email, would it feel like they were in connection with the same company?</p>
+          <p>Users experience your brand in many different ways. A visitor often feels like they're experiencing different companies when they interact with the business across multiple mediums; social media, website, email, etc.</p>
           <p>Studies show that <a href="http://www.uxbooth.com/articles/improve-conversions-with-better-personalization/" target="_blank">75% of consumers prefer when brands personalize</a> messages and offers, how does a business accomplish that if you're not capturing what the user is doing, integrating that information together, and making decisions based on those actions. If <a href="http://www.uxbooth.com/articles/improve-conversions-with-better-personalization/" target="_blank">70% of online consumers get frustrated</a> when promotional items they're offered has nothing to do with their interests, you're doing more harm than good by upsetting them by pushing generic promos into their experience.</p>
         </div>
         <div class="medium-5 columns">
           <div class="callout">
-            <h3 class="title" style=""><a name="help"></a>How can we help?</h3>
             <form accept-charset="UTF-8" action="https://qz130.infusionsoft.com/app/form/process/c29d90a7ef46142558c71f78dc5fd3a1" class="infusion-form" method="POST">              <div class="row">
                 <div class="small-12 columns">
                   <label class="" for="inf_field_FirstName">First Name *
@@ -213,7 +155,7 @@ Light Purple: ff99ff
                 </div>
                 <div class="small-12 columns">
                   <label class="" for="inf_misc_Message">Message *
-                    <textarea class="" rows="7" id="inf_misc_Message" name="inf_misc_Message" placeholder="Please help me help my users."></textarea>
+                    <textarea class="" rows="4" id="inf_misc_Message" name="inf_misc_Message" placeholder="Please help me help my users."></textarea>
                   </label>
 
                   <input name="inf_form_xid" type="hidden" value="c29d90a7ef46142558c71f78dc5fd3a1" />
---
+++ thank-you.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MarTech Zone | Solving Business Problems with Data</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <?php include_once('analytics.php'); ?>
+
+    <style>
+/*
+Orange: f69321
+Light Orange: fbcf9d
+
+Blue: 00aeef
+Light Blue: 99e4ff
+
+Purple: 660066
+Light Purple: ff99ff
+*/
+      .orange {color: #f69321;}
+      .blue {color: #00aeef;}
+      .purple {color: #660066;}
+      .red {color: #ff0000;}
+      .bkgd-gray {background-color: #f0f0f0;}
+
+      body, h1, h2, h3, h4, h5, h6 {font-family: 'Lato', sans-serif;}
+      p, li {font-size: 1.15rem;line-height: 2rem;}
+
+      a {color: #f69321;}
+      a:hover {color: #fbcf9d;}
+
+      .button {background: #f69321;border-radius: 8px;}
+      .button.hollow {color: #fff;border-color: #fff;}
+      .button:hover {background: #dc7a09;}
+      .button.hollow:hover {background: #fff;color: #f69321;border-color: #f69321;}
+
+      dl, ol, ul {list-style-position: inside;}
+
+      .callout {background: url('http://img.wallpaperfolder.com/f/4835D329375C/textura-bubbles-bolha-pt.jpg') 0 bottom;}
+      .top-bar {background: #f0f0f0;}
+    </style>
+  </head>
+
+  <body>
+    <!-- Start Top Bar -->
+    <nav class="top-bar">
+      <div class="top-bar-left">
+        <ul class="menu">
+          <li class="menu-text"><a href="/" style="padding: 0;"><img src="./imgs/MTZ-100h.png" style="max-height: 40px;"></a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- End Top Bar -->
+
+    <div style="padding: 2.5rem 0 30rem;">
+      <div class="row">
+        <div class="medium-12 columns">
+          <h2 class="text-center">Thank you!</h2>
+        </div>
+      </div>
+    </div>
+
+    <footer class="row column" style="">
+      <p style="padding: 1rem 0 0 1rem;">&copy; <?php echo date('Y'); ?> MarTech Zone, LLC</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
